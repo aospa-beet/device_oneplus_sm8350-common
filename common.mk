2fd45d8108a3cd17e2039785eb339b812fa078c6
Omkar Chandorkar <gotenksIN@aospa.co>
2023-05-30 14:44:50 +0900

    sm8350-common: switch to 1.2 QTI Bootctrl
    - introduce getActiveBootSlot
    
    Change-Id: I42e7b6b09808badade6982bc62295173e4e87aae
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- common.mk
+++ common.mk
@@ -81,9 +81,9 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Boot
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service
 
 # Camera
 $(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
